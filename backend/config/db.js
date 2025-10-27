6a0315f49ebe3ee4ee5d7cb4f956457a5a4a3cae
Dmitry <dmitry@smilecdr.com>
2025-10-26 21:34:13 -0600

    remove postgres url

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,7 +9,3 @@ const { PGUSER, PGPASSWORD, PGHOST, PGDATABASE } = process.env;
 export const sql = neon(
   `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require&channel_binding=require`
 );
-
-// this SQL function we export is used as a tagged template literal, which allows us to write SQL queries safely
-
-// postgresql://neondb_owner:npg_s4xE5hFKYbRX@ep-soft-hill-aa016sp8-pooler.westus3.azure.neon.tech/neondb?sslmode=require&channel_binding=require
